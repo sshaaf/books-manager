c42c9f3e413a35899a8ce745faa9955247c453c3
Shaaf, Syed <sshaaf@redhat.com>
2023-05-17 19:21:02 +0200

    add environment

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,41 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {Book} from "../model/book.model";
-
-const baseUrl = 'http://localhost:8080/books';
+import {environment} from "../../environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  baseUrl = environment.booksAPI;
+
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(baseUrl);
+    return this.http.get<Book[]>(this.baseUrl);
   }
 
   get(isbn: any): Observable<Book> {
-    return this.http.get<Book>(`${baseUrl}/${isbn}`);
+    return this.http.get<Book>(`${this.baseUrl}/${isbn}`);
   }
 
   create(data: any): Observable<any> {
-    return this.http.post(baseUrl, data);
+    return this.http.post(this.baseUrl, data);
   }
 
   update(isbn: any, data: any): Observable<any> {
-    return this.http.put(`${baseUrl}/${isbn}`, data);
+    return this.http.put(`${this.baseUrl}/${isbn}`, data);
   }
 
   delete(isbn: any): Observable<any> {
-    return this.http.delete(`${baseUrl}/${isbn}`);
+    return this.http.delete(`${this.baseUrl}/${isbn}`);
   }
 
   deleteAll(): Observable<any> {
-    return this.http.delete(baseUrl);
+    return this.http.delete(this.baseUrl);
   }
 
   findByTitle(title: any): Observable<Book[]> {
-    return this.http.get<Book[]>(`${baseUrl}?title=${title}`);
+    return this.http.get<Book[]>(`${this.baseUrl}?title=${title}`);
   }
 }
---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    booksAPI: 'http://localhost:8080/books',
+    authURL: 'http://localhost:8080/realms/quarkus'
+
+};
