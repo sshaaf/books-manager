62cadfdce0e59b31c0414371f4894e41318f71b7
Shaaf, Syed <sshaaf@redhat.com>
2023-05-25 19:26:39 +0200

    move to bookshelf

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'books-manager-angular'`, () => {
+  it(`should have as title 'bookshelf-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('books-manager');
+    expect(app.title).toEqual('bookshelf-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('books-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bookshelf-ui app is running!');
   });
 });
